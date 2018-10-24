d1be1a5076c33af72738a16f91afe768a3211118
Bjørnar Hagen <bjornar@datahjelpen.no>
2018-10-24 14:33:10 +0200

    Forgot to add StepButton component

---
+++ src/components/StepButton.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+
+class StepButton extends Component {
+  constructor(props) {
+    super(props);
+    this.nextStep = this.nextStep.bind(this);
+    this.previousStep = this.previousStep.bind(this);
+  }
+
+  nextStep() {
+    this.props.handler(this.props.step + 1);
+  }
+
+  previousStep() {
+    this.props.handler(this.props.step - 1);
+  }
+
+  render() {
+    return (
+      <button
+        className="continue"
+        onClick={() => {
+          if (this.props.direction === "previous") {
+            this.previousStep();
+          } else {
+            this.nextStep();
+          }
+        }}
+      >
+        {this.props.text}
+      </button>
+    );
+  }
+}
+
+export default StepButton;
