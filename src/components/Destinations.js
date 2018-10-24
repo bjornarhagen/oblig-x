7918aec3dd75b6ebd70fddc1ecdc5bb07cf8aedb
Bjørnar Hagen <bjornar@datahjelpen.no>
2018-10-24 14:34:59 +0200

    Updated Destinations. Added styling for component. Updated destinations list. Added images

--- src/components/Destinations.js
+++ src/components/Destinations.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+require("../scss/_components/destinations.scss");
 
 class Destinations extends Component {
   constructor(props) {
@@ -13,6 +14,7 @@ class Destinations extends Component {
   }
 
   state = {
+    destination: 0,
     destinations: []
   };
 
@@ -21,6 +23,7 @@ class Destinations extends Component {
     axios({ method: "get", baseURL: "/data/", url: "destinations.json" })
       .then(destinations => {
         this.setState({ destinations: destinations.data });
+        this.setDestination(this.state.destinations[this.state.destination]);
       })
       .catch(function(error) {
         console.error(error);
@@ -29,32 +32,53 @@ class Destinations extends Component {
 
   setDestination(destination) {
     this.props.handler(destination);
+
+    const elDestinations = document.querySelector("#destinations");
+    const elPlanetsWrapper = elDestinations.querySelector(".planets");
+    const elPlanets = elPlanetsWrapper.querySelectorAll(".planet");
+
+    for (let i = 0; i < elPlanets.length; i++) {
+      const planet = elPlanets[i];
+      planet.classList.remove("selected", "next", "previous");
+    }
+
+    const selectedPlanet = elPlanets[destination.id - 1];
+
+    selectedPlanet.classList.add("selected");
+
+    if (selectedPlanet.nextSibling) {
+      selectedPlanet.nextSibling.classList.add("next");
+    } else {
+      elPlanets[0].classList.add("next");
+    }
+
+    if (selectedPlanet.previousSibling) {
+      selectedPlanet.previousSibling.classList.add("previous");
+    } else {
+      elPlanets[elPlanets.length - 1].classList.add("previous");
+    }
   }
 
   render() {
     return (
-      <React.Fragment>
-        <h2>Destination</h2>
-        <table>
-          <thead>
-            <tr>
-              <th>Planet</th>
-              <th>Region</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.destinations.map(destination => (
-              <tr
-                key={destination.id}
-                onClick={() => this.setDestination(destination)}
-              >
-                <td>{destination.planet}</td>
-                <td>{destination.region}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </React.Fragment>
+      <div id="destinations">
+        <div className="planets">
+          {this.state.destinations.map(destination => (
+            <button
+              className="planet"
+              key={destination.id}
+              onClick={() => this.setDestination(destination)}
+              aria-label={"Velg " + destination.planet.name}
+            >
+              <h5 className="planet-name">{destination.planet.name}</h5>
+              <img
+                src={destination.planet.image}
+                alt={"Bilde av planeten " + destination.planet.name}
+              />
+            </button>
+          ))}
+        </div>
+      </div>
     );
   }
 }
