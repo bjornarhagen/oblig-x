ad041fedf610c3a8d55f403ea0a7e0366f55e371
Bjørnar Hagen <bjornar@datahjelpen.no>
2018-11-11 18:49:59 +0100

    Polyfill for forEach on NodeList (for IE 11)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import "./js/polyfill-nodelist-foreach";
+
 import "./css/index.css";
 import App from "./components/App";
 import GlobalHeader from "./components/GlobalHeader";
