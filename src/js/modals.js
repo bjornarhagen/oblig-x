487a5f148e79a65267b1e1d3faaf719ada98bff8
Bjørnar Hagen <bjornar@datahjelpen.no>
2018-11-13 19:16:05 +0100

    Fix modal script not picking up all the triggers

--- src/js/modals.js
+++ src/js/modals.js
@@ -1,62 +1,66 @@
-const modalTriggers = document.querySelectorAll(".modal-trigger");
+window.addEventListener("load", function() {
+  const modalTriggers = document.querySelectorAll(".modal-trigger");
 
-modalTriggers.forEach(modalTrigger => {
-  modalTrigger.addEventListener("click", openModal);
-});
+  console.log(modalTriggers);
 
-function openModal(evt) {
-  evt.preventDefault();
-  const modal = document.querySelector(this.getAttribute("data-target"));
+  modalTriggers.forEach(modalTrigger => {
+    modalTrigger.addEventListener("click", openModal);
+  });
 
-  if (modal) {
-    // Create a backdrop
-    let backdrop = document.createElement("div");
-    backdrop.classList.add("backdrop");
-    backdrop.modal = modal;
-    backdrop.backdrop = backdrop;
-    backdrop.addEventListener("click", closeModal);
+  function openModal(evt) {
+    evt.preventDefault();
+    const modal = document.querySelector(this.getAttribute("data-target"));
 
-    // Close modal when clicking an element with the modal-close class
-    const modalClosers = modal.querySelectorAll(".modal-close");
-    modalClosers.forEach(modalCloser => {
-      modalCloser.modal = modal;
-      modalCloser.backdrop = backdrop;
-      modalCloser.addEventListener("click", closeModal);
-    });
+    if (modal) {
+      // Create a backdrop
+      let backdrop = document.createElement("div");
+      backdrop.classList.add("backdrop");
+      backdrop.modal = modal;
+      backdrop.backdrop = backdrop;
+      backdrop.addEventListener("click", closeModal);
 
-    // Show the modal
-    modal.classList.add("show");
-    document.documentElement.appendChild(backdrop);
-    document.documentElement.classList.add("modal-active");
+      // Close modal when clicking an element with the modal-close class
+      const modalClosers = modal.querySelectorAll(".modal-close");
+      modalClosers.forEach(modalCloser => {
+        modalCloser.modal = modal;
+        modalCloser.backdrop = backdrop;
+        modalCloser.addEventListener("click", closeModal);
+      });
 
-    return true;
-  }
+      // Show the modal
+      modal.classList.add("show");
+      document.documentElement.appendChild(backdrop);
+      document.documentElement.classList.add("modal-active");
 
-  return false;
-}
-
-function closeModal(evt) {
-  evt.preventDefault();
-
-  if (this.modal) {
-    // Hide the modal
-    this.modal.classList.remove("show");
-    document.documentElement.classList.remove("modal-active");
-
-    // Delete the backdrop
-    if (this.backdrop) {
-      this.backdrop.removeEventListener("click", closeModal);
-      this.backdrop.parentElement.removeChild(this.backdrop);
+      return true;
     }
 
-    // Remove event listeners for the close modal buttons
-    const modalClosers = this.modal.querySelectorAll("modal-close");
-    modalClosers.forEach(modalCloser => {
-      modalCloser.removeEventListener("click", closeModal);
-    });
-
-    return true;
+    return false;
   }
 
-  return false;
-}
+  function closeModal(evt) {
+    evt.preventDefault();
+
+    if (this.modal) {
+      // Hide the modal
+      this.modal.classList.remove("show");
+      document.documentElement.classList.remove("modal-active");
+
+      // Delete the backdrop
+      if (this.backdrop) {
+        this.backdrop.removeEventListener("click", closeModal);
+        this.backdrop.parentElement.removeChild(this.backdrop);
+      }
+
+      // Remove event listeners for the close modal buttons
+      const modalClosers = this.modal.querySelectorAll("modal-close");
+      modalClosers.forEach(modalCloser => {
+        modalCloser.removeEventListener("click", closeModal);
+      });
+
+      return true;
+    }
+
+    return false;
+  }
+});
