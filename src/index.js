b0e2940683afc7b084008336e7a364b83494137b
Bjørnar Hagen <bjornar@datahjelpen.no>
2018-11-13 15:51:25 +0100

    Polyfill for Number.isNan

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "./js/polyfill-nodelist-foreach";
+import "./js/polyfill-number-isnan";
 
 import "./css/index.css";
 import App from "./components/App";
